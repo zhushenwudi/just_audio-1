6e36ce473b1e53eed2e57f04c4d0a51702f437a7
Ryan Heise <ryan@ryanheise.com>
2024-05-18 15:13:46 +1000

    Add setWebCrossOrigin to just_audio_background.

--- just_audio_background/lib/just_audio_background.dart
+++ just_audio_background/lib/just_audio_background.dart
@@ -292,6 +292,13 @@ class _JustAudioPlayer extends AudioPlayerPlatform {
       _playerAudioHandler.customSetShuffleOrder(request);
 
   @override
+  Future<SetWebCrossOriginResponse> setWebCrossOrigin(
+      SetWebCrossOriginRequest request) async {
+    _playerAudioHandler.customSetWebCrossOrigin(request);
+    return SetWebCrossOriginResponse();
+  }
+
+  @override
   Future<SeekResponse> seek(SeekRequest request) =>
       _playerAudioHandler.customPlayerSeek(request);
 
@@ -505,6 +512,11 @@ class _PlayerAudioHandler extends BaseAudioHandler
     return await (await _player).setShuffleOrder(SetShuffleOrderRequest(
       audioSourceMessage: _source!,
     ));
+  }
+
+  Future<SetWebCrossOriginResponse> customSetWebCrossOrigin(
+      SetWebCrossOriginRequest request) async {
+    return await (await _player).setWebCrossOrigin(request);
   }
 
   Future<ConcatenatingInsertAllResponse> customConcatenatingInsertAll(
